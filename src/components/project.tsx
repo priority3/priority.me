ef2f7636c7baa677b972675dec63da6ed017c680
priority <1832052104@qq.com>
2022-09-10 20:41:43 +0800

    feat: init project component

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,7 +1,12 @@
 export default function Project() {
   return (
     <div>
-      this is project
+      <div className="fbc w-full">
+          <h2 className="text-2xl font-600">Recent Projects</h2>
+          <a href="https://github.com/priority3">
+            <div className="i-fluent-emoji-film-projector cursor-pointer text-2xl" />
+          </a>
+      </div>
     </div>
   )
 }
