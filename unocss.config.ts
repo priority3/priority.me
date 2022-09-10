e5ed8fddcc9a466cb56be1a992671fa8da54955b
priority3 <prioritycc@outlook.com>
2022-09-10 20:28:21 +0800

    feat: add home link

--- unocss.config.ts
+++ unocss.config.ts
@@ -11,6 +11,11 @@ export default defineConfig({
   shortcuts: [
     {
       ...commonShortcuts,
+      linkBtnBase: `bg-light-200 rounded-md px-3 py-2 text-gray-700 cursor-pointer
+      hover:(text-white) transition-all duration-200 fcc gap-2`,
+      linkBtnGithub: 'hover:bg-[#3c3c3c]',
+      linkBtnBilibili: 'hover:bg-[#00a1d6]',
+      linkBtnBlogPosts: 'hover:bg-[#fd79a8]',
     },
   ],
   presets: [
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     Unocss(),
     react(),
   ],
+  resolve: {
+    alias: {
+      '@/': resolve(__dirname, 'src'),
+    },
+  },
 })
