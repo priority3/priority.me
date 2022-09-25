2c91dda17cf7433b0f74b5dfaa5c3065496cf765
priority3 <prioritycc@outlook.com>
2022-09-25 08:14:37 +0800

    fix: logo icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,26 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 export default function Header() {
+  const postProfileNameMap = {
+    '/': 'BlogPosts',
+    '/posts': 'Profile',
+  }
+  const postProfileUrlMap = {
+    '/': '/posts',
+    '/posts': '/',
+  }
+  const postProfileIconMap = {
+    '/': 'i-arcticons-blogger',
+    '/posts': 'i-arcticons-valkyrieprofile',
+  }
+
+  const [postProfile, setPostProfile] = useState({
+    name: 'BlogPosts',
+    url: '/posts',
+    icon: 'i-arcticons-blogger',
+  })
   const socialLinks = [
-    {
-      name: 'Posts',
-      url: '/posts',
-      icon: 'i-arcticons-blogger',
-    },
+    postProfile,
     {
       name: 'GitHub',
       url: 'https://github.com/priority3',
@@ -17,6 +32,15 @@ export default function Header() {
       icon: 'i-arcticons-bilibili',
     },
   ]
+  const location = useLocation()
+
+  useEffect(() => {
+    setPostProfile({
+      name: postProfileNameMap[location.pathname as keyof typeof postProfileNameMap],
+      url: postProfileUrlMap[location.pathname as keyof typeof postProfileUrlMap],
+      icon: postProfileIconMap[location.pathname as keyof typeof postProfileIconMap],
+    })
+  }, [location])
 
   return (
   <div className='w-full top-0 left-0 p-3 f-c  shadow-warm-gray-500 border-b-gray-2 border-b-2px' >
@@ -25,10 +49,13 @@ export default function Header() {
     >
       <nav>
           <Link to="/">
-            <div
-              className="text-dark-400 text-5 font-sans cursor-pointer"
-            >
-              priority
+            <div className='fcc gap-2'>
+              <div className='i-fluent-emoji-rolling-on-the-floor-laughing'></div>
+              <div
+                className="text-dark-400 text-5 font-sans cursor-pointer"
+              >
+                priority
+              </div>
             </div>
           </Link>
       </nav>
