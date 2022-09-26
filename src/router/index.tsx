77948be8f05788403360dff8aaa591e578492765
priority3 <prioritycc@outlook.com>
2022-09-26 10:12:50 +0800

    feat: genera route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,26 +1,11 @@
-import type { RouteObject } from 'react-router-dom'
+// import type { RouteObject } from 'react-router-dom'
 import { useRoutes } from 'react-router-dom'
-import Posts from '../views/posts'
 import Home from '../views/home'
-
-// async function getDynamicRoute(route: string) {
-//   const node = await import(`../views/${route}.tsx`)
-//   const { default: com } = node
-
-//   return com()
-// }
-// getDynamicRoute('home')
-
-const routes: RouteObject[] = [
-  {
-    path: '/',
-    element: <Home/>,
-  },
-  {
-    path: '/posts',
-    element: <Posts />,
-  },
-]
+import routes from '~react-pages'
+routes.push({
+  path: '/',
+  element: <Home/>,
+})
 
 export default function GeneratToRoute() {
   return (
@@ -28,4 +13,8 @@ export default function GeneratToRoute() {
     {useRoutes(routes)}
   </div>
   )
+}
+
+export {
+  routes,
 }
--- src/views/posts.tsx
+++ src/views/posts.tsx
@@ -1,14 +1,17 @@
 import Header from '../components/header'
+import PostsList from '@/components/postsList'
 
 export default function Posts() {
   return (
     <div>
       <Header />
-      <div className='fc w-full mt-20' >
+      <div className='f-c-c w-full gap-10 mt-20' >
         <h1 className="text-3xl">
           Priority.notes
         </h1>
+        <PostsList />
       </div>
+
     </div>
   )
 }
