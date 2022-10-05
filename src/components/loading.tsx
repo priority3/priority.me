1969e7c5328e05289cf07231bdc39b82258c82df
priority <1832052104@qq.com>
2022-10-05 19:21:09 +0800

    feat: add loading

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,9 +1,33 @@
+import '@/style/loading.scss'
 export default function Loading() {
   return (
-    <div>
-      <p>
-        this is loading...
-      </p>
+    <div id="section-preloader">
+      <div className="boxes">
+        <div className="box">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+        <div className="box">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+        <div className="box">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+        <div className="box">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
     </div>
   )
 }
