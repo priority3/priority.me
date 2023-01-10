cdf11670d402a03dc3bbf0eb9ec2dee7c4780eab
priority <1832052104@qq.com>
2023-01-10 09:54:55 +0800

    feat: leetcode sociallink

--- unocss.config.ts
+++ unocss.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     {
       ...commonShortcuts,
       ...projectShortcuts,
-      'linkBtnBase': `bg-light-200 rounded-md px-3 py-2 text-gray-700 cursor-pointer
+      'linkBtnBase': `bg-light-200 rounded-md px-3 py-2 text-gray-700 cursor-pointer h-10
       hover:(text-white) transition-all duration-200 fcc gap-2 my-2 md:m0`,
       'border-base': 'border-[#dfe4ea] dark:border-transparent',
       'linkBtnGithub': 'hover:bg-[#3c3c3c]',
@@ -26,6 +26,7 @@ export default defineConfig({
       'linkBtnTwitter': 'hover:bg-[#219ff5]',
       'linkBtnBlogPosts': 'hover:bg-[#fd79a8]',
       'linkBtnJuejin': 'hover:bg-[#a7ccfc]',
+      'linkBtnLeetcode': 'hover:bg-[#f7b500]',
       'box-hover': 'cursor-pointer hover:bg-[#88888808] hover:opacity-100 transition-all duration-300 rounded',
       // post list
       'over-desc': 'block w-[70%] md:max-w-[80%]',
