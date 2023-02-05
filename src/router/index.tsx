136ecd9600756602b06813e06ec855d446224299
priority <1832052104@qq.com>
2023-02-05 15:18:45 +0800

    feat: add back navgator

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,31 @@
 // import type { RouteObject } from 'react-router-dom'
-import { useRoutes } from 'react-router-dom'
+import { useLocation, useRoutes } from 'react-router-dom'
 import Home from '../views/home'
 import routes from '~react-pages'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 routes.push({
+  caseSensitive: true,
   path: '/',
   element: <Home/>,
 })
 
 export default function GeneratToRoute() {
+  const { pathname } = useLocation()
+
   return (
   <div>
     <Header />
       {useRoutes(routes)}
+      {
+        pathname !== '/' && pathname !== '/home'
+        && <div
+          className="mt-10 mb-6 m-auto opacity-50 fcc hover:op-75 cursor-pointer w-max"
+          onClick={() => window.history.back()}
+        >
+          cd..
+        </div>
+      }
     <Footer />
   </div>
   )
