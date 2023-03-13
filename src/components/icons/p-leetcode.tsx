5ac474cce80107a8ed01b37cbc2448af61e6f7a1
priority3 <prioritycc@outlook.com>
2023-03-13 22:26:46 +0800

    feat: leetcode github

---
+++ src/components/icons/p-leetcode.tsx
@@ -0,0 +1,6 @@
+export default function PLeetcode() {
+  // return isDark
+  //   ? <img className="mr-2 min-w-12 h-14" src="https://leetcode.com/_next/static/images/logo-dark-c96c407d175e36c81e236fcfdd682a0b.png" alt="" />
+  //   : <img className="mr-2 min-w-12 h-14" src="https://leetcode.com/_next/static/images/logo-ff2b712834cf26bf50a5de58ee27bcef.png" alt="" />
+  return <img className="mr-2 min-w-12 h-14" src="https://leetcode.com/_next/static/images/logo-ff2b712834cf26bf50a5de58ee27bcef.png" alt="" />
+}
